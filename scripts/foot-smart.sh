f0423d428f690fc147de58f65fbccbf100818661
arozoid <william.riwaka@gmail.com>
2026-08-29 13:47:33 +0800

    fix: foot --server not delayed enough

--- scripts/foot-smart.sh
+++ scripts/foot-smart.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-if ! pgrep -x foot >/dev/null; then
+if ! pgrep -xf "foot --server" >/dev/null; then
     foot --server >/dev/null 2>&1 &
+    sleep 0.05
 fi
 exec footclient "$@"
